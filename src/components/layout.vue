d70ef5baf7e7800e0c68dd4ea44edf8275058892
ivanyb <ivanyb@qq.com>
2017-11-21 09:42:51 +0800

    项目结构初始化代码

---
+++ src/components/layout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tmpl">
+        <!-- 1.0 左边菜单 -->
+
+        <!--2.0 顶部登录信息条 -->
+
+        <!-- 3.0 其他功能的渲染区 -->
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+<style scoped>
+</style>
