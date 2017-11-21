b51e7978b1cf90bdf99fe8ce6bf058e9872aa9e6
ivanyb <ivanyb@qq.com>
2017-11-21 16:22:31 +0800

    下午05 - 利用elementUI中的分页组件完成商品列表的分页功能.avi

--- src/components/goods/goodslist.vue
+++ src/components/goods/goodslist.vue
@@ -38,8 +38,7 @@
                 <!-- 表格区域
                 :data="list" 一定但是了一个数组，el-table就会自动遍历生成表格的每一个行数据
                 -->
-                <el-table ref="multipleTable" :data="list" border
-                 tooltip-effect="dark" style="width: 100%">
+                <el-table ref="multipleTable" :data="list" border tooltip-effect="dark" style="width: 100%">
                     <!-- type="selection":这一列是一个checkbox勾选框 -->
                     <el-table-column type="selection" width="55">
                     </el-table-column>
@@ -72,7 +71,15 @@
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="24"></el-col>
+            <el-col :span="24">
+                <!-- 分页 -->
+                <el-pagination @size-change="pageSizeChange"
+                 @current-change="pageIndexChange"
+                 :current-page="pageIndex" :page-sizes="[1,10, 20, 30, 40]"
+                :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
+                :total="totalCount">
+                </el-pagination>
+            </el-col>
         </el-row>
     </div>
 </template>
@@ -82,24 +89,49 @@
     export default {
         data() {
             return {
+                pageSize:10, //定义一个分页组件中页容量，默认是10条
+                pageIndex:1, //定义一个分页中的页索引，默认是第一页
+                totalCount:0, //数据api返回的数据总行数
                 // 搜索条件
                 searchValue: '',
                 list: []  // 用来存放商品数据数组，这个数组中的对象格式和/admin/goods/getlist响应回来的数据保持一致
             }
         },
         methods: {
+            // 2.0 分页相关的事件
+            // 获取当前的页索引 (pageindex)
+            pageIndexChange(currentPage){
+                console.log(currentPage);
+                // 将用户选择的当前页索引赋值给pageindex
+                this.pageIndex = currentPage;
+
+                // 重新获取新数据
+                this.getlist();
+            },
+            // 获取当前的页容量 （pagesize)
+            pageSizeChange(size){
+                console.log(size);
+                this.pageSize = size;
+
+                // 重新获取新数据
+                this.getlist();
+            },
             // 1.0 负责利用axios发出ajax请求：http://127.0.0.1:8899/admin/goods/getlist?pageIndex=1&pageSize=10&searchvalue=this.searchValue
             getlist() {
-            //    1.0.1 定义url 路径
-            var url = '/admin/goods/getlist?pageIndex=1&pageSize=10&searchvalue='+this.searchValue;
+                //    1.0.1 定义url 路径
+                var url = '/admin/goods/getlist?pageIndex='+this.pageIndex+'&pageSize='+this.pageSize+'&searchvalue=' + this.searchValue;
 
-            // 1.0.2 请求(axios)
-            this.$ajax.get(url).then(res=>{
-                this.list = res.data.message;
-            });
+                // 1.0.2 请求(axios)
+                this.$ajax.get(url).then(res => {
+                    // 将商品数据数组赋值
+                    this.list = res.data.message;
+
+                    // 将商品的总数据条数赋值
+                    this.totalCount = res.data.totalcount;
+                });
             }
         },
-        mounted(){
+        mounted() {
             // 当页面进入的时候就请求数据
             this.getlist();
         }
