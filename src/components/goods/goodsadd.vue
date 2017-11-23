952b5d1494811679a5bb0052242b53ed02c8ac6e
ivanyb <ivanyb@qq.com>
2017-11-23 15:02:56 +0800

    下午01 - 实现封面图片上传和相册图片上传功能.avi

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -24,9 +24,10 @@
                         <el-form-item label="所属类别">
                             <el-select v-model="ruleForm.category_id" placeholder="所属类别">
                                 <!-- 这些值一定是从数据api接口请求下来的 v-for来生成  el-option-->
-                                <el-option v-for="item in catelist" :value="item.category_id">
+                                <el-option v-for="item in catelist" :value="item.category_id" :label="item.title">
                                     <!-- 判断item.class_layer >1 就应该添加 |- -->
-                                 <span v-for="sub in (item.class_layer -1 )">&nbsp;</span> <span v-if="item.class_layer>1">|-</span>{{item.title}}
+                                    <span v-for="sub in (item.class_layer -1 )">&nbsp;</span>
+                                    <span v-if="item.class_layer>1">|-</span>{{item.title}}
                                 </el-option>
                             </el-select>
                         </el-form-item>
@@ -43,12 +44,19 @@
                             </el-switch>
                         </el-form-item>
                         <el-form-item label="封面上传">
-                            <el-upload class="upload-demo" action="#" :file-list="ruleForm.imgList" list-type="picture">
+                            <el-upload class="upload-demo"
+                             action="http://127.0.0.1:8899/admin/article/uploadimg"
+                              :file-list="ruleForm.imgList" list-type="picture"
+                              :on-success="imgUploaded">
                                 <el-button size="small" type="primary">点击上传</el-button>
                             </el-upload>
                         </el-form-item>
                         <el-form-item label="相册上传">
-                            <el-upload class="upload-demo" action="#" :file-list="ruleForm.fileList" list-type="picture">
+                            <el-upload class="upload-demo"
+                            action="http://127.0.0.1:8899/admin/article/uploadfile" 
+                            :file-list="ruleForm.fileList"
+                            :on-success="fileUplaoded"
+                            list-type="picture">
                                 <el-button size="small" type="primary">点击上传</el-button>
                             </el-upload>
                         </el-form-item>
@@ -73,7 +81,7 @@
                             vue-quill-editor 使用参考：http://172.16.2.23/vue/vueproject/project_admin.html#2
                             :content  ->类似于 v-model
                         -->
-                            <quill-editor :content="ruleForm.content">
+                            <quill-editor :content="ruleForm.content" @change="onEditorChange($event)">
                             </quill-editor>
                         </el-form-item>
 
@@ -104,10 +112,10 @@
                 catelist: [],  //存储分类下拉框中的数据
                 // 表单元素的双向数据绑定对象
                 ruleForm: {
-                    title: '',
-                    sub_title: '',
+                    title: '1',
+                    sub_title: '2',
                     "goods_no": "NZ0000000002",
-                    category_id: '',
+                    category_id: 43,  // 小问题：category_id要是一个数值类型，如果变成字符串类型的话，则页面显示不正常
                     "stock_quantity": 200,
                     "market_price": 1000,
                     "sell_price": 800,
@@ -118,18 +126,18 @@
                     "zhaiyao": "为不凡而生",
                     "content": "<p><strong>产品参数：</strong></p>",
                     "imgList": [
-                        {
-                            "name": "wTgAWDLpQReTQ-ZOMdlAk4vF.jpg",
-                            "url": "http://127.0.0.1:8899/imgs/wTgAWDLpQReTQ-ZOMdlAk4vF.jpg",
-                            "shorturl": "/imgs/wTgAWDLpQReTQ-ZOMdlAk4vF.jpg"
-                        }
+                        // {
+                        //     "name": "wTgAWDLpQReTQ-ZOMdlAk4vF.jpg",
+                        //     "url": "http://127.0.0.1:8899/imgs/wTgAWDLpQReTQ-ZOMdlAk4vF.jpg",
+                        //     "shorturl": "/imgs/wTgAWDLpQReTQ-ZOMdlAk4vF.jpg"
+                        // }
                     ],
                     "fileList": [
-                        {
-                            "name": "HN5d4_wrbsUk5KQNjzYSGGwm.jpg",
-                            "url": "http://127.0.0.1:8899/imgs/HN5d4_wrbsUk5KQNjzYSGGwm.jpg",
-                            "shorturl": "/imgs/HN5d4_wrbsUk5KQNjzYSGGwm.jpg"
-                        }
+                        // {
+                        //     "name": "HN5d4_wrbsUk5KQNjzYSGGwm.jpg",
+                        //     "url": "http://127.0.0.1:8899/imgs/HN5d4_wrbsUk5KQNjzYSGGwm.jpg",
+                        //     "shorturl": "/imgs/HN5d4_wrbsUk5KQNjzYSGGwm.jpg"
+                        // }
                     ]
                 },
                 // 表单元素的验证规则
@@ -146,6 +154,22 @@
             this.getcastlist();
         },
         methods: {
+            // 6.0 fileUplaoded 相册上传成功以后的回调
+            fileUplaoded(responese,file,filelist){
+                // 将responese通过 this.ruleForm.fileList.push(responese);才能保证多张图片
+                this.ruleForm.fileList.push(responese);
+            },
+            // 5.0 定义一个方法imgUploaded（当服务器成功响应以后回调）
+            imgUploaded(responese,file,filelist){
+                // 经过查看对比，responese就是一个和imgList数组中的对象格式一致的对象，所以只需要将这个对象使用[responese]
+                console.log(responese);
+                this.ruleForm.imgList = [responese];               
+            },
+            // 4.0 注册富文本编辑器的内容改变事件onEditorChange
+            onEditorChange(ev) {
+                console.log(ev);
+                this.ruleForm.content = ev.html;
+            },
             // 3.0 获取商品分类数据
             getcastlist() {
                 // 请求接口地址
@@ -164,7 +188,26 @@
                 this.$refs[formName].validate((valid) => {
                     // valid:返回true的话，表示表单元素全部合法，否则不合法
                     if (valid) {
-                        alert('提交成功');
+                        // 表示整个表单验证成功，应该将数据提交给服务器,完成新增工作
+                        this.$ajax.post('/admin/goods/add/goods', this.ruleForm)
+                            .then(res => {
+                                // 提示用户保存结果
+                                if (res.data.status == 1) {
+                                    // 提醒失败信息(使用了elementUI中的message消息提示)
+                                    this.$message({
+                                        showClose: true,
+                                        message: res.data.message,
+                                        type: 'error',
+                                        duration:1000
+                                    });
+                                }else{
+                                    // 跳转到列表页面
+                                    // this.$route : 代表接收路由对象，可以通过 this.$route.parmas获取参数
+                                    // this.$router：代表路由对象在vue上的一个原型对象可以调用他的push()方法完成页面的跳转
+                                    this.$router.push({ name: 'goodslist'});
+                                }
+                            });
+                        // console.log(this.ruleForm);
                     } else {
                         console.log('error submit!!');
                         return false;
